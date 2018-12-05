1353cd154f6ff89ab68761b70682242ba026dae3
Nick Lloyd <nick.lloyd@usask.ca>
2018-12-04 20:18:10 -0600

    Tweaked up the configure and make so it builds wheels properly

--- examples/test_umkehr.py
+++
@@ -1,6 +0,0 @@
-import umkehr.process
-
-inputfile  = '20091101.Dobson.Beck.119.JMA.csv'
-outputfile = '20091101.Dobson.Beck.119.JMA.Level2.csv'
-
-umkehr.process.Level1_to_Level2(inputfile,outputfile, KBN=3, verbose = True)
